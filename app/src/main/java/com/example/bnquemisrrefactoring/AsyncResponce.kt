9b24aac79962389680cd6d1234b6dbba58b2c169
Ehab Ebrahem <40669600+ehabebrahim@users.noreply.github.com>
2021-08-24 09:34:36 +0200

    Update AsyncResponce.kt

--- app/src/main/java/com/example/bnquemisrrefactoring/AsyncResponce.kt
+++ app/src/main/java/com/example/bnquemisrrefactoring/AsyncResponce.kt
@@ -2,7 +2,7 @@ package com.example.bnquemisrrefactoring
 
 interface AsyncResponce {
     fun processFinish(output: String?)
-
+//didn't understand how to map functions running in background.
     fun onclick(){
 
     }
